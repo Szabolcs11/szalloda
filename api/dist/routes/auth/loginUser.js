f8758f7fafb189785cf0c864d831ead2cab572da
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-10-01 18:50:17 +0200

    Updateing gitignore, files

--- api/dist/routes/auth/loginUser.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const router = (0, express_1.Router)({ mergeParams: true });
-router.get("/", async (req, res) => {
-    return res.send("Hello from loginUser");
-});
-exports.default = router;
