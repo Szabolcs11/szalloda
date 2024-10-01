9385fbd32ccf4552606e1292002c802824d8a320
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-10-01 23:14:57 +0200

    Update mysql

--- api/src/modules/helper/queryHelper.ts
+++ api/src/modules/helper/queryHelper.ts
@@ -4,7 +4,8 @@ import contprom from "../../modules/mysql";
 import bcrypt from "bcryptjs";
 
 export async function loginUser(username: string, password: string) {
-  const [res] = await (await contprom).query("SELECT * FROM users WHERE Username=?", [username]);
+  const [rows] = await contprom.query("SELECT * FROM users WHERE Username=?", [username]);
+  const res = rows as any[];
   if (res.length == 0) return false;
   if (!bcrypt.compareSync(password, res[0].Password)) {
     return false;
@@ -20,32 +21,36 @@ export async function createSession(userId: number, req: any) {
     Date: getDateTime(),
     Token: generateRandomString(32),
   };
-  const [res] = await (await contprom).query("INSERT INTO sessions SET ?", [info]);
+  const [rows] = await contprom.query("INSERT INTO sessions SET ?", [info]);
+  const res = rows as any[];
   if (!res) return false;
   return info.Token;
 }
 
 export async function checkSession(token: number) {
-  const [res] = await (await contprom).query("SELECT * FROM sessions WHERE Token=?", [token]);
+  const [rows] = await contprom.query("SELECT * FROM sessions WHERE Token=?", [token]);
+  const res = rows as any[];
   if (res.length == 0) return false;
   return res[0].UserId;
 }
 
 export async function getUserById(userId: number) {
-  const [res] = await (
-    await contprom
-  ).query(
+  const [rows] = await contprom.query(
     "SELECT users.id, users.Username, users.FullName, ranks.Name as RankName, ranks.Permission, users.CreatedAt FROM users INNER JOIN ranks ON users.Rank = ranks.id WHERE users.id=?",
     [userId]
   );
+  const res = rows as any[];
   if (res.length == 0) return false;
   return res[0];
 }
 
 export async function getStats() {
-  const [guests] = await (await contprom).query("SELECT COUNT(*) as count FROM guests");
-  const [rooms] = await (await contprom).query("SELECT COUNT(*) as count FROM rooms");
-  const [reservations] = await (await contprom).query("SELECT COUNT(*) as count FROM reservations");
+  const [guestRows] = await contprom.query("SELECT COUNT(*) as count FROM guests");
+  const guests = guestRows as any[];
+  const [roomRoows] = await contprom.query("SELECT COUNT(*) as count FROM rooms");
+  const rooms = roomRoows as any[];
+  const [reservationRoows] = await contprom.query("SELECT COUNT(*) as count FROM reservations");
+  const reservations = reservationRoows as any[];
 
   return {
     guests: guests[0].count,
@@ -55,15 +60,20 @@ export async function getStats() {
 }
 
 export async function getGuestById(id: number) {
-  const [res] = await (await contprom).query("SELECT * FROM guests WHERE id=? AND Deleted=0", [id]);
+  const [rows] = await contprom.query("SELECT * FROM guests WHERE id=? AND Deleted=0", [id]);
+  const res = rows as any[];
   if (res.length == 0) return false;
   return res[0];
 }
 
 export async function deleteGuest(id: number) {
-  const [res] = await (await contprom).query("UPDATE guests SET Deleted=1 WHERE id=?", [id]);
-  if (res.affectedRows == 0) return false;
-  return true;
+  const [rows] = await contprom.query("SELECT * FROM reservations WHERE GuestId=?", [id]);
+  const reservatios = rows as any[];
+  if (reservatios.length == 0) {
+    const [rows] = await contprom.query("UPDATE guests SET Deleted=1 WHERE id=?", [id]);
+    return true;
+  }
+  return false;
 }
 
 export async function createGuest(fullName: string, email: string, birthDate: string, phone: string) {
@@ -73,7 +83,8 @@ export async function createGuest(fullName: string, email: string, birthDate: st
     BirthDate: birthDate,
     Phone: phone,
   };
-  const [res] = await (await contprom).query("INSERT INTO guests SET ?", [info]);
+  const [rows] = await contprom.query("INSERT INTO guests SET ?", [info]);
+  const res = rows as any;
   if (!res.insertId) return false;
   return res.insertId;
 }
--- api/src/modules/mysql/index.ts
+++ api/src/modules/mysql/index.ts
@@ -1,12 +1,11 @@
 require("dotenv").config();
-const mysql = require("mysql2/promise");
+import mysql from "mysql2/promise";
 
 const contprom = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DB,
-  port: process.env.MYSQL_PORT,
   connectionLimit: 10,
 });
 
