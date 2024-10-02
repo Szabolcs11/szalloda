98725669feb6c6bf196de7d7076295a6a7a2bbf8
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-10-02 17:45:27 +0200

    Fixing login logic

--- api/src/routes/auth/loginUser.ts
+++ api/src/routes/auth/loginUser.ts
@@ -6,12 +6,13 @@ router.post("/", async (req: Request, res: any) => {
   const { username, password } = req.body;
   if (!username || !password) return res.status(200).send({ success: false, message: "Fill the fields!" });
   const user = await loginUser(username, password);
-  const token = await createSession(user.id, req);
   if (!user) return res.status(200).send({ success: false, message: "Invalid username or password!" });
+  const token = await createSession(user.id, req);
+  if (!user) return res.status(200).send({ success: false, message: "Unexpected error!" });
   return res
     .cookie("sessiontoken", token, { maxAge: 1000 * 60 * 60 * 24 * 7, secure: true })
     .status(200)
-    .send({ success: true, user });
+    .send({ success: true, message: "Successfully logged in!", user });
 });
 
 export default router;
