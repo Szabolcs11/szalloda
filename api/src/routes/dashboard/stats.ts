9cda9f77dc435d69cf81f6849322a50cb27082c1
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-10-02 22:13:24 +0200

    Update dashboard api

--- api/src/routes/dashboard/stats.ts
+++ api/src/routes/dashboard/stats.ts
@@ -1,10 +1,10 @@
-import { Router, Request } from "express";
-import { checkSession, createSession, getStats, getUserById, loginUser } from "../../modules/helper/queryHelper";
-const router = Router({ mergeParams: true });
+import { Request, Router } from "express";
+import { checkSession, getStats, getUserById } from "../../modules/helper/queryHelper";
+const router = Router();
 
 router.get("/", async (req: Request, res: any) => {
   const { sessiontoken } = req.cookies;
-  if (!sessiontoken) return res.status(200).send({ success: false, message: "No session token found!" });
+  if (!sessiontoken) return res.status(200).send({ success: false, message: "You are not logged in!" });
   const userId = await checkSession(sessiontoken);
   if (!userId) return res.status(200).send({ success: false, message: "Invalid session token!" });
   const stats = await getStats();
