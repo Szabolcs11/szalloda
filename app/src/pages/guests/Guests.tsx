594cd6a693f04e3afb22e58051a2b0b218880c2e
Szabolcs11 <kokeny.szabolcs04@gmail.com>
2024-11-06 22:29:15 +0100

    Adding search bar to guests

--- app/src/pages/guests/Guests.tsx
+++ app/src/pages/guests/Guests.tsx
@@ -8,13 +8,14 @@ import { navigator } from "../../navigator";
 function Guests() {
   const [isLoading, setIsLoading] = useState(true);
   const [guests, setGuests] = useState<Guest[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
   useEffect(() => {
     axios.get(ENDPOINTS.GUESTS).then((res) => {
       if (res.data.success) {
         setGuests(res.data.guests);
         setIsLoading(false);
       } else {
-        console.log(res.data)
+        console.log(res.data);
       }
     });
   }, []);
@@ -24,8 +25,21 @@ function Guests() {
   return (
     <div>
       <h1 className="title">Vendégek</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          value={searchQuery}
+          onChange={(e) => {
+            setSearchQuery(e.target.value);
+          }}
+          placeholder="Keresés"
+          className="search-input"
+        />
+      </div>
       <div className="rooms">
         {guests.map((e) => {
+          if (!e.FullName.includes(searchQuery) && !e.Email.includes(searchQuery) && !e.Phone.includes(searchQuery))
+            return null;
           return (
             <div key={e.id} className="room" onClick={() => navigator(`/guest/${e.id}`)}>
               <p style={{ fontSize: 18, fontWeight: "500" }}>{e.FullName}</p>
